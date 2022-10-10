dcf07406be896e657284378ba415cd488133023e
M7mmed-Sayed <mohamedsayed1167@gmail.com>
2022-10-10 22:52:51 +0200

    create Navbar

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+posts = [
+    {
+        'author': 'me',
+        'title': 'A',
+        'content': 'b',
+        'date_posted': 'August 27, 2019'
+    },
+    {
+        'author': 'me2',
+        'title': 'B',
+        'content': 'C',
+        'date_posted': 'August 5, 2019'
+    },
+]
+
+
+def home(request):
+    context = {'title': 'lol', 'posts':posts}
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')
